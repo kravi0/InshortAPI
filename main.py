06cec1f2993112a881b9f9dd624d7bce46c0d4c4
Ravi Kant Kumar <63406337+kravi0@users.noreply.github.com>
2024-01-14 00:56:23 +0530

    InshortAPI

---
+++ main.py
@@ -0,0 +1,25 @@
+from flask import Flask, request, jsonify
+from inshorts import getNews
+from flask_cors import CORS
+
+app = Flask(__name__)
+app.secret_key = "InshortAPI"
+CORS(app)
+
+@app.route('/')
+def home():
+    return 'API is working'
+
+@app.route('/news')
+def news():
+    if request.method == 'GET':
+        category = request.args.get("category")
+        if not category:
+            return jsonify({
+                "error": "please add category in query params"
+            }), 404
+        return jsonify(getNews(category)), 200
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host='0.0.0.0',port=5000,use_reloader=True)
